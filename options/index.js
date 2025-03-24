081292ab0c0eda6f4fd068bb127e216d0628e562
MaxHumpherys <maxhumpherys@gmail.com>
2025-03-23 23:04:39 -0600

    indented setting functionality. unchecking parent disables indented item

--- options/index.js
+++ options/index.js
@@ -1,17 +1,59 @@
 const checkAllBtn = document.getElementById('check-all');
-const radioBtns = document.getElementsByTagName('input');
+const checkboxBtns = document.getElementsByTagName('input');
 
-// Check / Uncheck buttons and change button text
-const checkButtons = (btnsToCheck, check) => {
+const usernameBtn = document.getElementById('username');
+const usernamePostGameBtn = document.getElementById('username-postgame')
+
+const ratingBtn = document.getElementById('rating');
+const ratingPostGameBtn = document.getElementById('rating-postgame');
+
+// Check / Uncheck checkboxes and change button text
+const checkAllButtons = (btnsToCheck, doCheck) => {
     for (let btn of btnsToCheck) {
-        btn.checked = check
+        btn.checked = doCheck;
     }
-    if (check) checkAllBtn.textContent = 'Uncheck All'
-    else if (!check) checkAllBtn.textContent = 'Check All'
+    toggleIndentButton(usernamePostGameBtn, usernameBtn)
+    toggleIndentButton(ratingPostGameBtn, ratingBtn);
 }
 
+// Refactor better way to handle conditional button switch
 checkAllBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (checkAllBtn.textContent == 'Check All') checkButtons(radioBtns, true)
-    else if (checkAllBtn.textContent == 'Uncheck All') checkButtons(radioBtns, false)
+    if (checkAllBtn.textContent == 'Check All') {
+        checkAllButtons(checkboxBtns, true);
+        checkAllBtn.textContent = 'Uncheck All';
+    }
+    else if (checkAllBtn.textContent == 'Uncheck All') {
+        checkAllButtons(checkboxBtns, false);
+        checkAllBtn.textContent = 'Check All';
+    }
 })
+
+const toggleIndentButton = (indentBtn, parentBtn) => {
+    console.log(indentBtn.parentElement)
+    indentBtn.disabled = !parentBtn.checked;
+    indentBtn.parentElement.previousElementSibling.style.opacity = indentBtn.disabled ? '.7' : '1';
+
+    if (indentBtn.disabled) indentBtn.checked = false;
+}
+
+ratingBtn.addEventListener('click', () => {
+    toggleIndentButton(ratingPostGameBtn, ratingBtn);
+})
+
+usernameBtn.addEventListener('click', () => {
+    toggleIndentButton(usernamePostGameBtn, usernameBtn)
+})
+
+
+
+
+
+
+
+// dev convenience, should pull storage.local and persist settings
+document.addEventListener('DOMContentLoaded', () => {
+    toggleIndentButton(usernamePostGameBtn, usernamePostGameLbl, usernameBtn)
+    toggleIndentButton(ratingPostGameBtn, ratingPostGameLbl, ratingBtn)
+})
+
