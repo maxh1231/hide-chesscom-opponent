d7b6e41dd36c04915ecd0ea97103f8769f42a227
max <maxhumpherys@gmail.com>
2025-04-20 23:58:31 -0600

    options js to reflect new structure

--- background/init-storage.js
+++ background/init-storage.js
@@ -1,6 +1,6 @@
 browser.runtime.onInstalled.addListener(() => {
     let userPreferences = {
-        enabled: true,
+        enable: true,
         postgame_rating: true,
         postgame_chat_gameover: true,
         postgame_chat_vote: true,
--- options/index.js
+++ options/index.js
@@ -1,103 +1,44 @@
-const checkAllBtn = document.getElementById('check-all');
 const checkboxBtns = document.getElementsByTagName('input');
-const submit = document.getElementById('submit');
 
 /**
- * Checks or unchecks all `btnsToCheck`.
- * @param {HTMLCollectionOf<HTMLInputElement>} btnsToCheck
- * @param {boolean} doCheck
+ * Enables or disables `btn` based on `activate`. Buttons that are disabled are also unchecked.
+ * @param {HTMLInputElement[]} btn
+ * @param {boolean} activate
  */
-const checkAllButtons = (btnsToCheck, doCheck) => {
-    for (let btn of btnsToCheck) {
-        btn.checked = doCheck;
-        if (btn.name.includes('postgame')) toggleIndentSetting(btn);
+const toggleBtn = (btn, activate) => {
+    if (activate) {
+        btn.disabled = false;
+        btn.parentElement.previousElementSibling.style.opacity = 1;
+    } else if (!activate) {
+        btn.disabled = true;
+        btn.checked = false;
+        btn.parentElement.previousElementSibling.style.opacity = 0.7;
     }
 };
 
 /**
- * Disables and unchecks input if parent input is unchecked.
- * Enables input if parent input is checked.
- * @param {HTMLInputElement} indentBtn
+ * Extension enable/disable event listener. Converts NodeList<HTMLInputElement> to HTMLInputElement[] and passes each button to `toggleBtn`.
  */
-const toggleIndentSetting = (indentBtn) => {
-    indentBtn.disabled =
-        !indentBtn.parentElement.parentElement.previousElementSibling
-            .firstElementChild.checked;
-    indentBtn.parentElement.previousElementSibling.style.opacity =
-        indentBtn.disabled ? '.7' : '1';
+document.getElementById('enable').addEventListener('click', (e) =>
+    Array.from(checkboxBtns)
+        .slice(1)
+        .forEach((btn) => toggleBtn(btn, e.target.checked))
+);
 
-    if (indentBtn.disabled) indentBtn.checked = false;
-};
-
-// Detects if all buttons are checked/unchecked and updates Check All button as needed
-// Refactor nested loop sucks
-// can possibly be combined with DOMContentLoaded
-for (const btn of checkboxBtns) {
-    btn.addEventListener('click', () => {
-        submit.disabled = false;
-
-        let allChecked = true;
-        let allUnchecked = true;
-
-        for (const btnEvent of checkboxBtns) {
-            if (!btnEvent.checked) allChecked = false;
-            if (btnEvent.checked) allUnchecked = false;
-            // btn.value
-        }
-
-        if (allChecked === true) checkAllBtn.textContent = 'Uncheck All';
-        if (allUnchecked === true) checkAllBtn.textContent = 'Check All';
-    });
-}
-
-// Refactor better way to handle conditional button switch
-checkAllBtn.addEventListener('click', (e) => {
-    submit.disabled = false;
-    e.preventDefault();
-    if (checkAllBtn.textContent == 'Check All') {
-        checkAllButtons(checkboxBtns, true);
-        checkAllBtn.textContent = 'Uncheck All';
-    } else if (checkAllBtn.textContent == 'Uncheck All') {
-        checkAllButtons(checkboxBtns, false);
-        checkAllBtn.textContent = 'Check All';
-    }
-});
-
-document
-    .getElementById('username')
-    .addEventListener('click', (e) =>
-        toggleIndentSetting(
-            e.target.parentElement.nextElementSibling.lastElementChild
-                .firstElementChild
-        )
-    );
-document
-    .getElementById('rating')
-    .addEventListener('click', (e) =>
-        toggleIndentSetting(
-            e.target.parentElement.nextElementSibling.lastElementChild
-                .firstElementChild
-        )
-    );
-
+/**
+ * Form submission event listener. Updates storage object with new values.
+ */
 document
     .getElementById('settings-form')
     .addEventListener('submit', async (e) => {
         e.preventDefault();
-        submit.disabled = true;
         let data = new FormData(e.target);
 
         let userPreferences = {
-            avatar: false,
-            badge: false,
-            flag: false,
-            flair: false,
-            grudge: false,
-            rating: false,
-            rating_postgame: false,
-            title: false,
-            username: false,
-            username_postgame: false,
+            enable: false,
+            postgame_rating: false,
+            postgame_chat_gameover: false,
+            postgame_chat_vote: false,
         };
 
         for (const key of data.keys()) {
@@ -116,10 +57,9 @@ document.addEventListener('DOMContentLoaded', async () => {
             await browser.storage.local.get('userPreferences');
 
         for (const [key, value] of Object.entries(userPreferences)) {
-            checkboxBtns[`${key}`].checked = value;
-
-            if (key.includes('postgame'))
-                toggleIndentSetting(checkboxBtns[`${key}`]);
+            if (!userPreferences.enable && key != 'enable')
+                toggleBtn(checkboxBtns[key], false);
+            checkboxBtns[key].checked = value;
         }
     } catch {
         console.error('User Preferences not found');
