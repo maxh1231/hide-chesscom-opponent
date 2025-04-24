322e0e253a383694c33f763f460e16325bb6e532
max <maxhumpherys@gmail.com>
2025-04-23 23:30:23 -0600

    CSS method instead of modifying CSS with JS

--- background/events.js
+++ background/events.js
@@ -1,13 +1,56 @@
+const stylesheets = [
+    'assets/stylesheets/base.css',
+    'assets/stylesheets/postgame-rating.css',
+    'assets/stylesheets/postgame-chat-gameover.css',
+    'assets/stylesheets/postgame-chat-vote.css',
+];
+
+/**
+ * Obtains User's preferences and builds an array of associated associated stylesheets.
+ * Returns null if extension is disbaled.
+ * @returns {string[] | null}
+ */
+const evalPreferences = async () => {
+    const { userPreferences } =
+        await browser.storage.local.get('userPreferences');
+    if (!userPreferences.enable) return null;
+    let arr = [];
+    Object.values(userPreferences).map((pref, i) => {
+        if (pref) arr.push(stylesheets[i]);
+    });
+
+    return arr;
+};
+
+/**
+ * Inserts or removes stylesheets.
+ * @param {string[]} sheets CSS to insert
+ * @param {number} tabId tab to insertCSS
+ * @param {'insert' | 'remove'} action to perform
+ */
+const applyCSS = (sheets, tabId, action) => {
+    console.log(sheets, tabId, action);
+    browser.scripting[`${action}CSS`]({
+        target: {
+            tabId: tabId,
+        },
+        files: sheets,
+    });
+};
+
 browser.webNavigation.onHistoryStateUpdated.addListener(
-    (e) => {
-        browser.tabs.sendMessage(e.tabId, { event: 'url' });
+    async (e) => {
+        const preferences = await evalPreferences();
+        if (preferences) applyCSS(preferences, e.tabId, 'insert');
     },
     { url: [{ urlMatches: 'https://www.chess.com/game/*' }] }
 );
 
 browser.storage.onChanged.addListener(async () => {
     let [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-    if (tab && tab.url.includes('chess.com')) {
-        browser.tabs.sendMessage(tab.id, { event: 'storage' });
-    }
+    if (!tab.url || !tab.url.startsWith('https://www.chess.com/game')) return;
+    applyCSS(stylesheets, tab.id, 'remove');
+
+    const preferences = await evalPreferences();
+    if (preferences) applyCSS(preferences, tab.id, 'insert');
 });
--- hide-opponent.js
+++ hide-opponent.js
@@ -43,36 +43,38 @@ const validateElement = (selector, callback, attempts = 1) => {
     }
 };
 
-browser.runtime.onMessage.addListener(async () => {
-    const { userPreferences } =
-        await browser.storage.local.get('userPreferences');
-    if (!userPreferences.enable) return;
-    toggleElement(document.querySelector('div.user-tagline-component'));
-    toggleElement(document.querySelector('div.player-avatar-component > img'));
-    document
-        .querySelectorAll('div.game-start-message-component')
-        .forEach((el) => {
-            toggleElement(el);
-        });
-
-    if (userPreferences.postgame_rating)
-        validateElement('div.player-tagline', (element) => {
-            const elementsToHide = ['div.player-game-over-component'];
-            createObserver(element, elementsToHide);
-        });
-
-    if (
-        userPreferences.postgame_chat_gameover ||
-        userPreferences.postgame_chat_vote
-    )
-        validateElement('div.chat-room-chat', (element) => {
-            const elementsToHide = [
-                userPreferences.postgame_chat_gameover &&
-                    'div.game-over-message-component',
-                userPreferences.postgame_chat_vote &&
-                    'div.game-rate-sport-message-component',
-            ].filter(Boolean);
-
-            createObserver(element, elementsToHide);
-        });
+browser.runtime.onMessage.addListener(async (message) => {
+    // const { userPreferences } =
+    //     await browser.storage.local.get('userPreferences');
+    // console.log(userPreferences);
+    // if (message.event === 'storage' && !userPreferences.enable) {
+    //     return;
+    // } else if (!userPreferences.enable) return;
+    // toggleElement(document.querySelector('div.user-tagline-component'));
+    // toggleElement(
+    //     document.querySelector('div.player-avatar-component.player-avatar')
+    // );
+    // document
+    //     .querySelectorAll('div.game-start-message-component')
+    //     .forEach((el) => {
+    //         toggleElement(el);
+    //     });
+    // if (userPreferences.postgame_rating)
+    //     validateElement('div.player-tagline', (element) => {
+    //         const elementsToHide = ['div.player-game-over-component'];
+    //         createObserver(element, elementsToHide);
+    //     });
+    // if (
+    //     userPreferences.postgame_chat_gameover ||
+    //     userPreferences.postgame_chat_vote
+    // )
+    //     validateElement('div.chat-room-chat', (element) => {
+    //         const elementsToHide = [
+    //             userPreferences.postgame_chat_gameover &&
+    //                 'div.game-over-message-component',
+    //             userPreferences.postgame_chat_vote &&
+    //                 'div.game-rate-sport-message-component',
+    //         ].filter(Boolean);
+    //         createObserver(element, elementsToHide);
+    //     });
 });
